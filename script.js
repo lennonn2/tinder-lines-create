f78cdd3fab4206ef0e7b36835850db34f3345608
Niall Lennon <niall.lennon2@gmail.com>
2018-05-08 23:02:40 -0700

    add new elements

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ function submit() {
   // endpoints:
   // POST - https://qvqnyun7pl.execute-api.us-east-1.amazonaws.com/prod/notes
   // GET - https://qvqnyun7pl.execute-api.us-east-1.amazonaws.com/prod/lines
-  const message = document.getElementById('messages').value;
-  const messages = message.split(',');
+  const messageContainer = document.getElementById('messageContainer');
+  const messageElements = messageContainer.getElementsByClassName('messageObject');
+  const messages = [...messageElements]
+    .map((container, index) => {
+      const checked = container.querySelector('input[type=checkbox]').checked;
+      return {
+        text: container.querySelector('.message').value,
+        index: index,
+        isReply: checked
+      };
+    });
 
   const categories = [];
   if (document.getElementById('funny').checked) categories.push('funny');
@@ -19,15 +28,30 @@ function submit() {
   };
 
   const url = 'https://qvqnyun7pl.execute-api.us-east-1.amazonaws.com/prod/notes';
-  fetch(url, {
-    body: JSON.stringify(data), // must match 'Content-Type' header
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, same-origin, *omit
-    headers: {
-      'content-type': 'application/json'
-    },
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, cors, *same-origin
-  })
-  .then(response => document.getElementById('response').innerText = response);
+  // fetch(url, {
+  //   body: JSON.stringify(data), // must match 'Content-Type' header
+  //   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+  //   credentials: 'same-origin', // include, same-origin, *omit
+  //   headers: {
+  //     'content-type': 'application/json'
+  //   },
+  //   method: 'POST', // *GET, POST, PUT, DELETE, etc.
+  //   mode: 'cors', // no-cors, cors, *same-origin
+  // })
+  // .then(response => document.getElementById('response').innerText = response);
+}
+
+function add() {
+  const container = document.getElementById('messageContainer');
+  const div = document.createElement('div');
+  div.classList.add('messageObject');
+  const text = document.createElement('input');
+  text.classList.add('message');
+  const checkbox = document.createElement('input');
+  checkbox.setAttribute('type', 'checkbox');
+  div.appendChild(text);
+  div.appendChild(checkbox);
+  div.appendChild(document.createTextNode('Reply'));
+  div.appendChild(document.createElement('br'));
+  container.appendChild(div);
 }
